e8e902904055c6ebeb9a0242e5f234fff56d3134
Dimentium <dk@lemd.cc>
2026-09-06 09:45:35 +0200

    Update Homebrew formula to 0.1.5

--- Formula/macmcp.rb
+++ Formula/macmcp.rb
@@ -1,7 +1,7 @@
 class Macmcp < Formula
   desc "Local macOS MCP bridge for mail, Calendar, and Reminders"
   homepage "https://github.com/Dimentium/macmcp"
-  url "https://github.com/Dimentium/macmcp.git", tag: "v0.1.4", revision: "a5c6079fbe2dab2cdc43ca4a3fa4fbc6f61c557c"
+  url "https://github.com/Dimentium/macmcp.git", tag: "v0.1.5", revision: "1bd50766b9740fce6c41dcb733337a08dd6d0de8"
   license "MIT"
 
   def install
