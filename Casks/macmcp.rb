dcc41bf19c3df6f40480e5d05941aa47a52c9188
Dimentium <dk@lemd.cc>
2026-09-07 23:07:04 +0200

    Publish MacMCP 0.2.17 Cask

--- Casks/macmcp.rb
+++ Casks/macmcp.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "macmcp" do
-  version "0.2.16"
-  sha256 "5047b36ba2f786c631884d1904a7552f95567518a6e6ecaed4b4ebbe0c6401dc"
+  version "0.2.17"
+  sha256 "e25d3178090b8647d288e4f54d085cb97f6f5678788075f7b38be393cdf7324d"
 
   url "https://github.com/Dimentium/macmcp/releases/download/v#{version}/MacMCP-#{version}-macos.zip"
   name "MacMCP"
--- Formula/macmcp.rb
+++ Formula/macmcp.rb
@@ -1,7 +1,7 @@
 class Macmcp < Formula
   desc "Local macOS MCP bridge for mail, Calendar, and Reminders"
   homepage "https://github.com/Dimentium/macmcp"
-  url "https://github.com/Dimentium/macmcp.git", tag: "v0.2.16", revision: "38f1ef7e20abcaf542f0c21dfc678215e5e736fe"
+  url "https://github.com/Dimentium/macmcp.git", tag: "v0.2.17", revision: "5b9a0aa3778da505d1bbf4fa49ef4704662a6519"
   license "MIT"
 
   depends_on "go"
