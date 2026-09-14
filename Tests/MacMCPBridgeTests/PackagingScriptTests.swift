f52c749e1127e88190e4ded680b6004d491a893f
Dimentium <dk@lemd.cc>
2026-09-14 22:10:09 +0200

    Prepare MacMCP 0.2.30

--- Tests/MacMCPBridgeTests/PackagingScriptTests.swift
+++ Tests/MacMCPBridgeTests/PackagingScriptTests.swift
@@ -321,9 +321,9 @@ final class PackagingScriptTests: XCTestCase {
             encoding: .utf8
         )
 
-        XCTAssertTrue(appVersion.contains("static let version = \"0.2.29\""))
-        XCTAssertTrue(appInfo.contains("<string>0.2.29</string>"))
-        XCTAssertEqual(bundleInfo.components(separatedBy: "<string>0.2.29</string>").count, 3)
+        XCTAssertTrue(appVersion.contains("static let version = \"0.2.30\""))
+        XCTAssertTrue(appInfo.contains("<string>0.2.30</string>"))
+        XCTAssertEqual(bundleInfo.components(separatedBy: "<string>0.2.30</string>").count, 3)
     }
 
     func testLocalArchiveScriptExcludesWorkspaceArtifacts() throws {
